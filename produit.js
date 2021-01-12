4aeef946366bcd67f69c4e9601f4e8551312c916
SDucatte <sebastien.ducatte@gmail.com>
2021-01-12 13:40:09 +0100

    ajout fichier produit.js

--- produit.js
+++ produit.js
@@ -1,18 +1,18 @@
-appelAjax({}).then((result) => {
-    let div = document.getElementById('one-product');
+let urlId = window.location.search;
+let urlParams = new URLSearchParams(urlId);
+let oneId = urlParams.get('id');
+console.log(oneId);
+
+showProduct({}).then((result) => {
+    let otherDiv = document.getElementById('product-content');
 
     for (let product of result) {
-        var card = `<a class='card link' href='produit.html?id=${product._id}'>
-            <h2 class='card-header'>${product.name}</h2>
+    var oneProduct = `<h2 class='card-header'>${product.name}</h2>
             <img class='image card-img-top' src='${product.imageUrl}'>
             <p class='card-subtitle'>${product.description}</p>
             <p class='card-text'>${product.price} €</p>
         </a>`;
 
-        div.innerHTML += card;
-    }
-    
+        otherDiv.innerHTML += oneProduct;
+}
 })
-
-const getUrl = window.location.search;
-console.log(getUrl);
