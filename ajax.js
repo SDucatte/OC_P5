815f867f26f4c68e45280f3d18f5e1c46d6df803
SDucatte <sebastien.ducatte@gmail.com>
2021-01-12 13:45:15 +0100

    modifications

--- ajax.js
+++ ajax.js
@@ -1,7 +1,4 @@
 // Requete AJAX générique
-
-
-
 function appelAjax(options) {
 var defaultOption = {
     "api" : "/furniture", 
@@ -34,7 +31,36 @@ options = Object.assign(defaultOption, options);
 
 // Requete affichage du produit
 
-
+function showProduct(options) {
+    var defaultOption = {
+        "api" : "/furniture", 
+        "method" : "GET", 
+        "status" : 200, 
+        "data" : null, 
+        "idProduct" : urlParams.get('id'),
+        "listHeader" : []
+    }
+    
+    options = Object.assign(defaultOption, options);
+        return new Promise ((resolve, reject) => {
+           let request = new XMLHttpRequest();
+       
+           request.onreadystatechange = function() {
+               if (this.readyState == XMLHttpRequest.DONE && this.status == options.status) {
+                   resolve(JSON.parse(this.responseText));
+                 
+               } else if (this.readyState == XMLHttpRequest.DONE && this.status != options.status) {
+                   reject(JSON.parse(this.responseText));
+               }
+           };
+           for (let header of options.listHeader){
+            request.setRequestHeader(header.name, header.value);
+        }
+    
+           request.open(options.method, "http://localhost:3000/api" + options.api);
+           request.send();
+       })
+    }
 
 
 
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 appelAjax({}).then((result) => {
     let div = document.getElementById('product-content');
 
@@ -15,9 +14,24 @@ appelAjax({}).then((result) => {
     
 })
 
-const getUrl = window.location;
-console.log(getUrl);
+let urlId = window.location.search;
+let urlParams = new URLSearchParams(urlId);
+let oneId = urlParams.get('id');
+console.log(oneId);
 
+showProduct({}).then((result) => {
+    let otherDiv = document.getElementById('product-content');
+
+    for (let product of result) {
+    var oneProduct = `<h2 class='card-header'>${product.name}</h2>
+            <img class='image card-img-top' src='${product.imageUrl}'>
+            <p class='card-subtitle'>${product.description}</p>
+            <p class='card-text'>${product.price} €</p>
+        </a>`;
+
+        otherDiv.innerHTML += oneProduct;
+}
+})
 
 
 
